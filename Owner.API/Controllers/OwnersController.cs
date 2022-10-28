f92fcae1ed90b473d5b057f7ef71b20e45f67e03
HilalBatibeyi <batibeyihilal@gmail.com>
2022-10-29 00:55:52 +0300

    Add project files.

---
+++ Owner.API/Controllers/OwnersController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Owner.API.Data;
+using Owner.API.Model;
+using System.Linq;
+
+namespace Owner.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnersController : ControllerBase
+    {
+        [Route("All")]
+        [HttpGet]
+        public IActionResult GelAllOwner()
+        {
+            var result = new OwnerData().GelAllOwner();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Consumes("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OwnerModel))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Add(OwnerModel owner)
+        {
+            var result = new OwnerData().GelAllOwner();
+
+            if (owner.Description.Contains("hack"))
+            {
+                return BadRequest("Invalid description");
+            }
+
+            result.Add(owner);
+            return Ok(result);
+        }
+
+        [Route("Update")]
+        [HttpPut]
+        public IActionResult Update(int id, OwnerModel owner)
+        {
+            if (id != owner.Id)
+                return BadRequest("owner id numbers did not match");
+
+            var result = new OwnerData().GelAllOwner();
+            var ownerToUpdate = result.FirstOrDefault(x => x.Id == id);
+
+            ownerToUpdate.Name=owner.Name;
+            ownerToUpdate.LastName = owner.LastName;
+            ownerToUpdate.Description=owner.Description;
+            ownerToUpdate.Date = owner.Date;
+            ownerToUpdate.Description = owner.Description;
+            ownerToUpdate.Type = owner.Type;
+
+            return Ok(ownerToUpdate);
+        }
+
+        [Route("Delete")]
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var result = new OwnerData().GelAllOwner();  
+            var ownerToDelete = result.FirstOrDefault(x => x.Id == id);
+
+            if (ownerToDelete == null)
+                return NotFound($"Owner {id} not found.");
+
+            result.Remove(ownerToDelete);
+            return Ok("owner deleted");
+        }
+    }
+}
---
+++ Owner.API/Model/OwnerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Owner.API.Model
+{
+    public class OwnerModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+        public string Type { get; set; }
+    }
+}
